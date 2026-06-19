29272273feb0e40417cada547498d78acb97a454
LyHong569 <lhong1@puthisastra.edu.kh>
2026-06-19 11:16:49 +0700

    enhance: add bottom sheet to app theme and locale change, fix initial flash when change locale

--- app/src/main/java/com/example/testandroid/features/profile/presentation/component/locale/ProfileLocaleBottomSheet.kt
+++ app/src/main/java/com/example/testandroid/features/profile/presentation/component/locale/ProfileLocaleBottomSheet.kt
@@ -2,19 +2,23 @@ package com.example.testandroid.features.profile.presentation.component.locale
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.RadioButton
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -23,20 +27,31 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.testandroid.cores.models.AppLanguages
 import com.example.testandroid.cores.models.Language
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileLocaleBottomSheet(
     localeViewModel: ProfileLocaleViewModel = hiltViewModel()
 ) {
+    val sheetState = rememberModalBottomSheetState()
+    var showBottomSheet by remember { mutableStateOf(false) }
     val settingState by localeViewModel.localeState.collectAsStateWithLifecycle()
 
-    val onAppLanguageChanged: (String) -> Unit = { newLanguage ->
-        localeViewModel.changeLanguage(newLanguage)
+    Button(onClick = { showBottomSheet = true }) {
+        Text("Open")
     }
 
-    SettingContent(
-        selectedLanguage = settingState.selectedLanguage,
-        onAppLanguageChanged = onAppLanguageChanged
-    )
+    if (showBottomSheet) {
+        ModalBottomSheet(
+            onDismissRequest = { showBottomSheet = false },
+            sheetState = sheetState,
+        ) {
+            SettingContent(
+                selectedLanguage = settingState.selectedLanguage,
+                onAppLanguageChanged = localeViewModel::changeLanguage
+            )
+        }
+    }
+
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -45,21 +60,15 @@ fun SettingContent(
     selectedLanguage: String,
     onAppLanguageChanged: (String) -> Unit,
 ) {
-    Scaffold { paddingValues ->
-        Box(
-            modifier = Modifier.padding(paddingValues)
-        ) {
-            LazyColumn {
-                items(AppLanguages) { language ->
-                    LanguageRow(
-                        language = language,
-                        isSelected = language.code == selectedLanguage,
-                        onClick = {
-                            onAppLanguageChanged(language.code)
-                        }
-                    )
+    LazyColumn {
+        items(AppLanguages) { language ->
+            LanguageRow(
+                language = language,
+                isSelected = language.code == selectedLanguage,
+                onClick = {
+                    onAppLanguageChanged(language.code)
                 }
-            }
+            )
         }
     }
 }
--- app/src/main/java/com/example/testandroid/features/profile/presentation/component/theme/ProfileThemeBottomSheet.kt
+++ app/src/main/java/com/example/testandroid/features/profile/presentation/component/theme/ProfileThemeBottomSheet.kt
@@ -2,36 +2,55 @@ package com.example.testandroid.features.profile.presentation.component.theme
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.RadioButton
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.example.testandroid.cores.models.ThemeMode
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ProfileThemeBottomSheet(
     themeViewModel: ProfileThemeViewModel = hiltViewModel()
 ) {
+    val sheetState = rememberModalBottomSheetState()
+    var showBottomSheet by remember { mutableStateOf(false) }
     val settingState by themeViewModel.themeState.collectAsState()
 
-    SettingContent(
-        selectedTheme = settingState.selectedTheme,
-        onAppThemeChanged = themeViewModel::setTheme
-    )
+    Button(onClick = { showBottomSheet = true }) {
+        Text("Open")
+    }
+
+    if (showBottomSheet) {
+        ModalBottomSheet(
+            onDismissRequest = { showBottomSheet = false },
+            sheetState = sheetState,
+        ) {
+            SettingContent(
+                selectedTheme = settingState.selectedTheme,
+                onAppThemeChanged = themeViewModel::setTheme
+            )
+        }
+    }
+
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -40,19 +59,13 @@ fun SettingContent(
     selectedTheme: ThemeMode,
     onAppThemeChanged: (ThemeMode) -> Unit,
 ) {
-    Scaffold { paddingValues ->
-        Box(
-            modifier = Modifier.padding(paddingValues)
-        ) {
-            LazyColumn {
-                items(ThemeMode.entries.toTypedArray()) { theme ->
-                    ThemeRow(
-                        theme = theme,
-                        isSelected = theme == selectedTheme,
-                        onClick = { onAppThemeChanged(theme) }
-                    )
-                }
-            }
+    LazyColumn {
+        items(ThemeMode.entries.toTypedArray()) { theme ->
+            ThemeRow(
+                theme = theme,
+                isSelected = theme == selectedTheme,
+                onClick = { onAppThemeChanged(theme) }
+            )
         }
     }
 }
